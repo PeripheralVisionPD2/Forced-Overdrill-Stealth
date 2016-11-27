fd41a2c8bf20c2d850eb7e2a4348e98ef160def5
PeripheralVisionPD2 <jaypoopys@gmail.com>
2016-11-27 12:23:47 -0500

    Delete warning.lua

--- warning.lua
+++
@@ -1,9 +0,0 @@
-function MenuManager:show_warning_mod(var)
-	local dialog_data = {}
-	dialog_data.title = "Overdrill Stealth"
-	dialog_data.text = " This session has Overdrill Stealth Enabled, DONACDUM. \n This mod is 99% of DrNewbies code 1% of mine!"
-	local ok_button = {}
-	ok_button.text = managers.localization:text("dialog_ok")
-	dialog_data.button_list = {ok_button}
-	managers.system_menu:show(dialog_data)
-end
